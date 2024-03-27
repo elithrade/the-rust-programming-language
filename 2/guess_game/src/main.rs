ab54b66e6b805fbbc65ae9f99693a5ad901ba586
Bin Hu <bin.hu@rac.com.au>
2024-03-27 15:31:26 +0800

    chapter 2 guessing game 2: move inside loop and error handling

--- 2/guess_game/src/main.rs
+++ 2/guess_game/src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::io;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!");
@@ -8,33 +8,46 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // By default variables are immutable, adding mut make it muttable.
-    let mut guess = String::new();
+        // By default variables are immutable, adding mut make it muttable.
+        let mut guess = String::new();
 
-    io::stdin()
-        // & indicate the argument is a reference which means multiple parts of the code
-        // access the same variable without copying the data multiple times.
-        .read_line(&mut guess)
-        // read_line returns Result type which can be in multiple states, the Ok state idicates the
-        // operation was successful, Err states meaning the operation has failed.
-        // If Result is Err, expect will cause the program to crash and display the message passed
-        // in.
-        .expect("Failed to read line");
+        io::stdin()
+            // & indicate the argument is a reference which means multiple parts of the code
+            // access the same variable without copying the data multiple times.
+            .read_line(&mut guess)
+            // read_line returns Result type which can be in multiple states, the Ok state idicates the
+            // operation was successful, Err states meaning the operation has failed.
+            // If Result is Err, expect will cause the program to crash and display the message passed
+            // in.
+            .expect("Failed to read line");
 
-    // guess variable here is called Shadowing, it lets us to reuse the variable.
-    // trim() to remove the newline character in the input string.
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // guess variable here is called Shadowing, it lets us to reuse the variable.
+        // trim() to remove the newline character in the input string.
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        //
+        // Handling invalid input. We switched from expect call to a match expression to move from
+        // crashing on error to handling the error.
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    // A match expression made up of arms.
-    // An arm consist of a pattern to match against, and the code that should be run if the value
-    // given to match fits the pattern.
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small!"),
-        std::cmp::Ordering::Greater => println!("Too big!"),
-        std::cmp::Ordering::Equal => println!("You win!"),
+        // A match expression made up of arms.
+        // An arm consist of a pattern to match against, and the code that should be run if the value
+        // given to match fits the pattern.
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                // Exiting the loop when match euqals.
+                break;
+            }
+        }
+
+        println!("You guessed: {guess}");
     }
-
-    println!("You guessed: {guess}");
 }
