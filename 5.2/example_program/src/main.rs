e8dacaebb6e4d4fea7934ac2f46590dd5a5bf382
Bin HU <elithrade@gmail.com>
2024-04-04 20:20:26 +0800

    chapter 5.2 debug trait and derive attribute

---
+++ 5.2/example_program/src/main.rs
@@ -0,0 +1,55 @@
+// Adding the attribute to derive the Debug trait and printing the
+// Rectangle instance using debug formatting.
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    println!("rect1 is {:?}", rect1);
+
+    // we can use {:#?} instead of {:?} in the println! string.
+    // In this example, using the {:#?} style will output the following:
+    // Note that println! only takes the reference, not ownership.
+    println!("rect1 is {:#?}", rect1);
+
+    // dbg! macro takes the ownership, prints the line number and source code, then returns the
+    // ownership.
+    // We don't want dbg! to take the ownership of rect1, so we use reference to rect1.
+    dbg!(&rect1);
+
+    dbg_ownership();
+}
+
+fn dbg_ownership() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        // dgb! borrows and returns the value to width.
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    println!("--- dbg!(&rect1) ---");
+    dbg!(&rect1);
+
+    println!("--- dbg!(rect1) ---");
+    dbg!(rect1);
+
+    // This won't compile because the ownership is taken.
+    // println!("rect1 is {:?}", rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
