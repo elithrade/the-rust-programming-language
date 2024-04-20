c6d9cb9f579f618136f751c03d1b118562cc6b71
Bin HU <elithrade@gmail.com>
2024-04-20 09:47:43 +0800

    chapter 10.1 generics

---
+++ 10/function-extracting/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
+
+fn largest(number_list: &Vec<i32>) -> &i32 {
+    let mut largest = &number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
