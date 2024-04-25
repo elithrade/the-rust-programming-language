7d85e011775f711ead1f09cdbcff87110972b9ea
Bin HU <elithrade@gmail.com>
2024-04-25 09:20:21 +0800

    chapter 12 handles error from run function

--- 12/minigrep/src/main.rs
+++ 12/minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     // collect() turns iterator into a vector.
@@ -13,9 +13,21 @@ fn main() {
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With text:\n{contents}")
+    // if let rather than unwrap_or_else to check errors from run function.
+    // Since run doesnot return value we only handles the case when there is an error.
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // ? will return the errors from current function for the caller to handle.
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
