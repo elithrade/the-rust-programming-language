c797e8f0e52e52926dce478ee969c09260e10366
Bin Hu <elithrade@gmail.com>
2024-05-09 20:58:00 +0800

    chapter 17.3 implementing request_review on Post and State trait

--- 17.3/state-pattern/src/lib.rs
+++ 17.3/state-pattern/src/lib.rs
@@ -4,10 +4,40 @@ pub struct Post {
     content: String,
 }
 
+impl Post {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        ""
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+}
+
 // State trait defines the behaviour shared by different post states.
-trait State {}
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+}
 
 // The default state of a Post.
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
