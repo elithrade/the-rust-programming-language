431c5febe81360b722e38c25790b3879edfee05b
Bin Hu <bin.hu@rac.com.au>
2024-05-13 14:05:40 +0800

    chapter 20 handle request and send response

--- 20/hello/src/main.rs
+++ 20/hello/src/main.rs
@@ -1,11 +1,32 @@
-use std::net::TcpListener;
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    // incoming method returns an iterator that gives us s sequence of streams.
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|x| !x.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
